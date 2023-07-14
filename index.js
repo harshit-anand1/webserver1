3ce722d7d05e9662017b9d8be9320b967965f022
Harshit Anand <iamharshit101@gmail.com>
2023-07-14 13:16:55 +0530

    server created

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 const http = require('http');
+
+const server = http.createServer((req, res)=>{
+    res.end("server is up and running");
+})
+server.listen(8000,()=>{
+    console.log("The server is running")
+});
